2185bf60a57f8f68d4e3033364c4af7512f2bd3e
Penny Tague <pmtague9@gmail.com>
2020-03-13 09:32:00 -0700

    Initial commit, ex1 complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,23 +1,38 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
-from hashtables import (HashTable,
-                        hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+from hashtables import (
+	HashTable,
+	hash_table_insert,
+	hash_table_remove,
+	hash_table_retrieve,
+	hash_table_resize
+	)
 
 
 def get_indices_of_item_weights(weights, length, limit):
-    ht = HashTable(16)
+	ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+	# Add weights and their indeces to the hashtable
+	for item in range(0, length):
+		hash_table_insert(ht, weights[item], item)
+	
+	# Check the hashtable for the value that equals the limit minus the weight in the weights list
+	for index in range(0, length):
+		difference = limit - weights[index]
 
-    return None
+		# Check for difference in hashtable
+		check_hashtable = hash_table_retrieve(ht, difference)
+
+		if check_hashtable is not None:
+			if check_hashtable > index:
+				return (check_hashtable, index)
+			else:
+				return (index, check_hashtable)
+
+	return None
 
 
 def print_answer(answer):
-    if answer is not None:
-        print(str(answer[0] + " " + answer[1]))
-    else:
-        print("None")
+	if answer is not None:
+		print(str(answer[0] + " " + answer[1]))
+	else:
+		print("None")
